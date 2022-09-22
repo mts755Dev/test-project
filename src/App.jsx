75c9695531a83448a7442ea8ade281235ddbf9ac
talha saleem <Talha.saleem@devsinc.com>
2022-09-22 18:52:35 +0500

    Link to home button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Routes} from 'react-router-dom';
+import {Route, Routes, Link} from 'react-router-dom';
 import Projects from './components/Projects/Projects';
 import Head from './components/Head/Head';
 import Bugs from './components/Bugs/Bugs';
@@ -8,9 +8,11 @@ import BugShow from './components/Bugs/BugShow';
 function App() {
   return (
       <div className="App">
-        <Head />
+        <nav>
+          <Link className='text-decoration-none' to="/" ><Head /></Link>
+        </nav>
         <Routes>
-          <Route exact path="/"  element={<Projects />}/>
+          <Route path="/"  element={<Projects />}/>
           <Route  path="/:id/bugs" element={<Bugs />}/>
           <Route  path="/:id/bugs/:bugId" element={<BugShow />} />
         </Routes>
